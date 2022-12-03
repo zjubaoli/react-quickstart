f3732bade04c88adbabe806dc1a7011225895239
zjubaoli <344024908@qq.com>
2022-12-03 11:19:45 +0800

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+const user = {
+  firstName: 'bao',
+  lastName: 'li',
+};
+
+function format(user) {
+  return user.firstName + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+
+    return element;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+
+setInterval(getGreeting, 1000);
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return getGreeting(user);
 }
